8db4992a8e0b1ae17edec6e47667ae179108c868
vgroupdev-dang <hadangktmt@gmail.com>
2018-01-26 15:28:43 +0700

    add client components

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App'
+import reducers from './reducers'
+
+//the first param is the initial reducer
+//the second param is the initial state
+//the 3rd param is what middleware we use (can be thunk or saga or what ever)
+const store = createStore(reducers, {}, applyMiddleware())
+
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>,
+    document.getElementById('root')
+);
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
-const mongoose = require('mongoose')    
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
 
 const User = mongoose.model('users')
@@ -20,20 +20,14 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
-    proxy: true    
-}, 
-(accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id })  
-        .then((existingUser) => {
-            if (existingUser) {
-                done(null, existingUser)
-            } else {
-                new User({ googleId: profile.id })
-                    .save()
-                    .then(user => done(null, user))
-            }
-        })
-        
-    console.log(profile)
-    
-}))
+    proxy: true
+},
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id })
+        if (existingUser) {
+            return done(null, existingUser)
+        }
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user)
+
+    }))

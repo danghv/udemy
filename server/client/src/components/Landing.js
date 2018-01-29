ae38673a79b163b3998f982c01de5e7dcf71d510
vgroupdev-dang <hadangktmt@gmail.com>
2018-01-29 18:09:28 +0700

    update

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+        <div>
+        <h1>Landing</h1>
+        Collect feedback from ur users
+        </div>
+    )
+}
+
+export default Landing

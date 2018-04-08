5caf5cc965e5b3a7764854ebf2e39181da8e26d0
danghv <hadangktmt@gmail.com>
2018-04-08 17:15:04 +0700

    change git ignore

---
+++ server/config/dev.js
@@ -0,0 +1,9 @@
+// prod.js - production keys here!!
+module.exports = {
+    googleClientID: '989443014060-0n6tf971op6p0p25niht8oil1h81jkt8.apps.googleusercontent.com',
+    googleClientSecret: '8KHUq7GZcxOWcEE_VRvgaH4H',
+    mongoURI: 'mongodb://hadang:hadang@ds163806.mlab.com:63806/emaily-dev-mrd',
+    cookieKey: 'hd4ever',
+    stripePublishableKey: 'pk_test_9OBu7EleSmZe5Scnx200euUd',
+    stripeSecretKey: 'sk_test_2PXLJa54pLPO08oZ9FZXV8Dd'
+}
